384a1e29394c6ca6c5a802843f0005995199134d
Asia <asyayakimova@yahoo.com>
2025-05-14 13:24:58 +0200

    Tiny fix

--- Order.Domain/OrderItem.cs
+++ Order.Domain/OrderItem.cs
@@ -2,5 +2,7 @@ namespace Order.Domain;
 
 public class OrderItem
 {
-    
+    public Guid ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
 }
