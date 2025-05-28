ec678a356ef8d5799e52fafc2681402c3e40e4c9
Asia <asyayakimova@yahoo.com>
2025-05-28 13:02:30 +0200

    Test for connection string

---
+++ ECommerce.Tests/PostgreeConnectionTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ECommerce.Tests;
+
+public class PostgreeConnectionTest
+{
+    [Fact]
+    public void TestDbConnection()
+    {
+        //arrange
+        //act
+        //assert
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .Build();
+        var connectionString = config.GetConnectionString("DefaultConnection")
+                               ?? "Host=localhost;Database=ecommerce;Username=postgres;Password=12345678"; 
+
+        Assert.NotEmpty(connectionString);
+        Assert.True(!string.IsNullOrEmpty(connectionString));
+    }
+}
