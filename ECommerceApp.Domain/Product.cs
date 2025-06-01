8df5043306f26805cb0a4ab9e4e0949bcb8ff5d7
asyayakim <asyayakimova@yahoo.com>
2025-06-01 20:23:09 +0200

    Updated name to avoid conflict

---
+++ ECommerceApp.Domain/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace ECommerceApp.Domain;
+
+public class Product
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public DateTime OrderDate { get; private set; } = DateTime.UtcNow;
+    public List<OrderItem> Items { get; private set; } = new();
+    private Product() {}
+    public DateTime CreatedAt { get; private set; }
+    public Product(Guid customerId)
+    {
+        Id = Guid.NewGuid();
+        CustomerId = customerId;
+        CreatedAt = DateTime.UtcNow;
+    }
+    public void AddProductItem(Guid productId, int quantity, decimal unitPrice)
+    {
+        Items.Add(new OrderItem
+            (productId, quantity, unitPrice));
+    }
+}
